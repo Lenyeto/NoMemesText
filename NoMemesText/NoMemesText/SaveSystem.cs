b083da45cb54ebefda89a4c086fca34098ab1ffb
William Douglas Buell <patg.lenyeto@gmail.com>
2017-02-28 13:42:16 -0500

    Add a way to save player data

--- NoMemesText/NoMemesText/SaveSystem.cs
+++ NoMemesText/NoMemesText/SaveSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NoMemesText
 {
@@ -12,6 +13,8 @@ namespace NoMemesText
         {
             Dictionary<string, User> tmp = UserHandler.getInstance().getUsers();
 
+            FileStream fs = File.Create("../media/saves.sav");
+
             foreach (KeyValuePair<string, User> u in tmp)
             {
                 //Console.WriteLine(u.Key);
@@ -19,9 +22,27 @@ namespace NoMemesText
                 //Console.WriteLine(u.Value.level);
                 Console.WriteLine(u.Key);
                 Console.WriteLine(u.Value.ToString());
+
+                string tmp2 = u.Key + '\t' + u.Value.ToString();
+
+                byte[] tmpBytes = Encoding.Unicode.GetBytes(tmp2.ToCharArray());
+
+                fs.Write(tmpBytes, 0, tmpBytes.Length);
+                
             }
+
+            fs.Close();
         }
 
+        public static void loadData()
+        {
+            string[] lines = File.ReadAllLines("../media/saves.sav");
+
+            foreach (string s in lines)
+            {
+                string[] tmp = s.Split('\t');
+            }
+        }
         
     }
 }
