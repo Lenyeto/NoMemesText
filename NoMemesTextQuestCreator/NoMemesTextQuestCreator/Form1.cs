2865180731485ebef00bb21b96e7497927dd31d6
BrutalTostitos <josh.wynn27@gmail.com>
2017-03-07 20:35:39 -0500

    Quest Log setup
    Added in a seperate quest creation class. Created a basic spreadsheet in
    the necessary format. TODO: parse through the spreadsheet

---
+++ NoMemesTextQuestCreator/NoMemesTextQuestCreator/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoMemesTextQuestCreator
+{
+    public partial class Form1 : Form
+    {
+
+
+        Quest tmpQuest;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tmpQuest.mName = QuestNameBox.Text;
+        }
+    }
+}
