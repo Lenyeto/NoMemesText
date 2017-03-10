b1d1cddcc55cd818bebdd15e4d25e04bc6c70bd7
William Douglas Buell <patg.lenyeto@gmail.com>
2017-03-10 00:49:21 -0500

    Changed to VS 2017 and readded Quest.cd

---
+++ NoMemesText/NoMemesText/Quest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoMemesTextQuestCreator
+{
+    enum questCondition { Deliver, Kill, Gather, Goto };
+
+    enum creatureType { Beast, Humanoid, Undead, Elemental };
+
+    public class Quest
+    {
+        public string mName;
+        public string mDescription;
+
+        public int mXPReward;
+        public int mGoldReward;
+
+        public int mItemIDReward;
+
+        public int mAmountToKill;
+
+        questCondition mCond;
+
+        creatureType mCreatureToKill;
+
+        
+
+        //Coords placeToKill;
+
+        public Quest(string name, string description, int XPReward = 0, int goldReward = 0, int itemIDReward = 0)
+        {
+            mName = name;
+            mDescription = description;
+            mXPReward = XPReward;
+            mGoldReward = goldReward;
+            mItemIDReward = itemIDReward;
+        }
+    }
+}
